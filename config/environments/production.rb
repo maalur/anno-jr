6ec1de1ebbf4cc4642bce6a53a57b6cd76e77ff9
maalur <markurich@gmail.com>
2015-02-16 20:59:00 -0800

    make all dependencies local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ AnnoJr::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( annojr.js )
+  config.assets.precompile += %w( annojr.js, annoj.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
